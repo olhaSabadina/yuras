7a143cb94b8aaa6580afcb0f58ce1e325a0bea40
yuraSud <sabadin85@icloud.com>
2023-03-24 19:34:40 +0200

    16786786

---
+++ yura test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yura test
+//
+//  Created by YURA																			 on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
